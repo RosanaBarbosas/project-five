a3eea61884f51216986ad9d697bfad02b022ecc4
Rosana Barbosa dos Santos <rosana_b_santos@hotmail.com>
2025-08-17 19:39:51 +0100

    botao que ao ser clicado criara um histograma

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+
+car_data = pd.read_csv('vehicles.csv')
+hist_button = st.button("Criar histograma")
+
+if hist_button:
+    st.write("Criando um histograma para o conjunto de dados de anuncios de venda de carros")
+    
+    fig = px.histogram(car_data, x="odometer")
+
+    st.plotly_chart(fig, use_container_width=True)
